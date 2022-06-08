c556c1fe853751c61cef57e57d97dcc900505db0
pilsnerish <18150523+pilsnerish@users.noreply.github.com>
2022-06-08 17:16:05 -0400

    Add files via upload

--- CL650_xsaitekpanels.lua
+++ CL650_xsaitekpanels.lua
@@ -4,48 +4,46 @@
 -- (at your option) any later version.
 
 if AIRCRAFT_FILENAME == "CL650.acf" then
-	dataref("SIM_VERTICAL_VELOCITY", "sim/cockpit/autopilot/vertical_velocity", "writable")
+	dataref("CL650_xsaitekpanels_Vertical_Velocity", "sim/cockpit/autopilot/vertical_velocity", "writable")
 
-	dataref("XSP_F1", "bgood/xsaitekpanels/sharedata/float1", "writable")
-	-- dataref("XSP_F2", "bgood/xsaitekpanels/sharedata/float2", "writable")
-	dataref("XSP_F3", "bgood/xsaitekpanels/sharedata/float3", "writable")
-	dataref("XSP_F4", "bgood/xsaitekpanels/sharedata/float4", "writable")
-	dataref("XSP_F5", "bgood/xsaitekpanels/sharedata/float5", "writable")
+	dataref("CL650_xsaitekpanels_Float1", "bgood/xsaitekpanels/sharedata/float1", "writable")
+--	dataref("CL650_xsaitekpanels_Float2", "bgood/xsaitekpanels/sharedata/float2", "writable")
+	dataref("CL650_xsaitekpanels_Float3", "bgood/xsaitekpanels/sharedata/float3", "writable")
+	dataref("CL650_xsaitekpanels_Float4", "bgood/xsaitekpanels/sharedata/float4", "writable")
+	dataref("CL650_xsaitekpanels_Float5", "bgood/xsaitekpanels/sharedata/float5", "writable")
 
-	dataref("CL650_ARINC429_FCC_0_ALT_SEL", "abus/CL650/ARINC429/FDCU-1/words/FCC/0/ALT_SEL")
-	dataref("CL650_ARINC429_FCC_0_VS_TGT",  "abus/CL650/ARINC429/FDCU-1/words/FCC/0/VS_TGT")
-	dataref("CL650_ARINC429_FCC_0_IAS_SEL", "abus/CL650/ARINC429/FDCU-1/words/FCC/0/IAS_SEL")
-	dataref("CL650_ARINC429_FCC_0_HDG_SEL", "abus/CL650/ARINC429/FDCU-1/words/FCC/0/HDG_SEL")
-	dataref("CL650_ARINC429_FCC_0_CRS_SEL", "abus/CL650/ARINC429/FDCU-1/words/FCC/0/CRS_SEL")
+	dataref("CL650_xsaitekpanels_FCC_0_ALT_SEL",	"abus/CL650/ARINC429/L-FCC-1/words/FCC/0/ALT_SEL")
+	dataref("CL650_xsaitekpanels_FCC_0_VS_TGT_TGT",	"abus/CL650/ARINC429/L-FCC-1/words/FCC/0/VS_TGT_TGT")
+	dataref("CL650_xsaitekpanels_FCC_0_IAS_SEL",	"abus/CL650/ARINC429/L-FCC-1/words/FCC/0/IAS_SEL")
+	dataref("CL650_xsaitekpanels_FCC_0_HDG_SEL",	"abus/CL650/ARINC429/L-FCC-1/words/FCC/0/HDG_SEL")
+	dataref("CL650_xsaitekpanels_FCC_0_CRS_SEL",	"abus/CL650/ARINC429/L-FCC-1/words/FCC/0/CRS_SEL")
 
-	function CL650_m2ft(m)
+	function CL650_xsaitekpanels_m2ft(m)
 		if (not(m ~= m)) then
 			local ft = m * 3.2808399
-			ft = math.ceil(ft)
-			ft = ft - (ft % 10)
-			return ft
+			return math.floor(ft + 0.5)
 		end
 	end
 
-	function CL650_mps2fpm(mps)
+	function CL650_xsaitekpanels_mps2fpm(mps)
 		if (not(mps ~= mps)) then
 			return mps * 3.2808399 * 60
 		end
 	end
 
-	function CL650_mps2kts(mps)
+	function CL650_xsaitekpanels_mps2kts(mps)
 		if (not(mps ~= mps)) then
 			return mps * 1.9438444924406
 		end
 	end
 
-	function CL650_ARINC429_FCC_to_XSP()
-		XSP_F1 = CL650_m2ft(CL650_ARINC429_FCC_0_ALT_SEL)
-		SIM_VERTICAL_VELOCITY = CL650_mps2fpm(CL650_ARINC429_FCC_0_VS_TGT)
-		XSP_F3 = CL650_mps2kts(CL650_ARINC429_FCC_0_IAS_SEL)
-		XSP_F4 = CL650_ARINC429_FCC_0_HDG_SEL
-		XSP_F5 = CL650_ARINC429_FCC_0_CRS_SEL
+	function CL650_xsaitekpanels_FCC_to_XSP()
+		CL650_xsaitekpanels_Float1 = CL650_xsaitekpanels_m2ft(CL650_xsaitekpanels_FCC_0_ALT_SEL)
+		CL650_xsaitekpanels_Vertical_Velocity = CL650_xsaitekpanels_mps2fpm(CL650_xsaitekpanels_FCC_0_VS_TGT_TGT)
+		CL650_xsaitekpanels_Float3 = CL650_xsaitekpanels_mps2kts(CL650_xsaitekpanels_FCC_0_IAS_SEL)
+		CL650_xsaitekpanels_Float4 = CL650_xsaitekpanels_FCC_0_HDG_SEL
+		CL650_xsaitekpanels_Float5 = CL650_xsaitekpanels_FCC_0_CRS_SEL
 	end
 
-	do_every_frame("CL650_ARINC429_FCC_to_XSP()")
+	do_every_frame("CL650_xsaitekpanels_FCC_to_XSP()")
 end
